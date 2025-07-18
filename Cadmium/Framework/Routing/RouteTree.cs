a35dc442cd52bd11ae4c01a940c41837c76f3e74
arestinum <patrikas.lyddon@gmail.com>
2025-07-18 16:16:38 +0100

    Turn the BuildTree method into a recursive method

--- Cadmium/Framework/Routing/RouteTree.cs
+++ Cadmium/Framework/Routing/RouteTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace Cadmium.Framework.Routing
@@ -13,52 +14,41 @@ namespace Cadmium.Framework.Routing
         {
             RelativeRootDirectoryPath = rootDirectoryPath;
             RootDirectoryPath = rootDirectoryPath;
-            BuildTree();
+            Root = BuildTree(rootDirectoryPath);
         }
 
         public string RelativeRootDirectoryPath;
         public string RootDirectoryPath;
         public virtual RouteNode Root { get; set; }
 
-        public void BuildTree()
+        public RouteNode BuildTree(string path)
         {
-            string[] childrenPaths = Directory.GetDirectories(RootDirectoryPath);
-            IEnumerable<RouteMetadata> routeMetadatas = childrenPaths.Select(childPath =>
-            {
-                if (File.Exists(Path.Combine(childPath, "\\metadata.json")))
-                    return JsonSerializer.Deserialize<RouteMetadata>(File.ReadAllText(Path.Combine(childPath, "\\metadata.json")));
-
-                return null;
-            });
+            string folderName = path.Split('\\').Last();
+            string[] childrenPaths = Directory.GetDirectories(path);
 
             RouteMetadata metadata = null;
-            string metadataFilePath = $"{RootDirectoryPath}\\metadata.json";
+            string metadataFilePath = $"{path}\\metadata.json";
             bool hasMetadataFile = File.Exists(metadataFilePath);
             if (hasMetadataFile)
-                metadata = JsonSerializer.Deserialize<RouteMetadata>(File.ReadAllText($"{RootDirectoryPath}\\metadata.json"), new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                metadata = JsonSerializer.Deserialize<RouteMetadata>(File.ReadAllText($"{path}\\metadata.json"), new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
 
-            Root = new RouteNode()
+            RouteNodeConditions conditions = new RouteNodeConditions 
             {
-                AbsolutePath = metadataFilePath,
-                FolderName = RootDirectoryPath.Split('\\').Last(),
-                Children = childrenPaths.Select(childPath => {
-                    return new RouteNode()
-                    {
-                        AbsolutePath = childPath,
-                    }; 
-                }).ToList(),
-                Condition = new RouteNodeConditions
-                {
-                    HasDefault = File.Exists(
-                        Path.Combine(RootDirectoryPath, "\\Default.aspx")
-                    ),
-                    HasDynamicRoute = childrenPaths
+                HasDefault = File.Exists($"{path}\\Default.aspx"),
+                HasDynamicRoute = childrenPaths
                         .Select(childPath => {
                             string childPathName = childPath.Split('\\').Last();
                             return childPathName.StartsWith("[") && childPathName.EndsWith("]");
                         })
                         .Contains(true),
-                },  
+            };
+
+            return new RouteNode()
+            {
+                AbsolutePath = path,
+                FolderName = folderName,
+                Children = childrenPaths.Select(childPath => BuildTree(childPath)).ToList(),
+                Condition = conditions,  
                 RouteParameters = new Dictionary<string, string>(),
                 Metadata = metadata
             };
